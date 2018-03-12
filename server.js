a15bdffd79315cb896c0e0f85960ac724cf62d2e
fchow2018 <dong_chow@yahoo.com>
2018-03-12 15:21:23 -0700

    edited server.js to include my own personal API

--- server.js
+++ server.js
@@ -47,14 +47,13 @@ app.get('/api', function apiIndex(req, res) {
   // It would be seriously overkill to save any of this to your database.
   // But you should change almost every line of this response.
   res.json({
-    woopsIForgotToDocumentAllMyEndpoints: true, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
-    documentationUrl: "https://github.com/example-username/express-personal-api/README.md", // CHANGE ME
-    baseUrl: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    documentationUrl: "https://github.com/fchow2018/express-personal-api/README.md",
+    baseUrl: "https://polar-ridge-40446.herokuapp.com/",
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
+      {method: "GET", path: "/api/profile", description: "Who I am"},
+      {method: "POST", path: "/api/nhlteams", description: "Create a new NHL team"}
     ]
   })
 });
